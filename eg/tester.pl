7b985a3a420c8eeb63ca9511a8f7d7c6ccb26277
Gene Boggs <gene@ology.net>
2025-03-13 20:06:53 -0700

    WIP

--- eg/tester.pl
+++ eg/tester.pl
@@ -22,7 +22,7 @@ my $rtfg = MIDI::RtController::Filter::Gene->new(rtc => $rtc);
 my $rtfm = MIDI::RtController::Filter::Math->new(rtc => $rtc);
 
 $rtfm->delay(0.2);
-$rtfm->feedback(4);
+$rtfm->feedback(3);
 
 # add_filters('pedal', $rtfg->curry::pedal_tone, 0);
 add_filters('stair', $rtfm->curry::stair_step, 0);
