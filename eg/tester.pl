2c490a101b89853ebd20ff1b2eb8ada6fb0316c0
Gene Boggs <gene@ology.net>
2025-04-20 18:36:34 -0700

    WIP

--- eg/tester.pl
+++ eg/tester.pl
@@ -20,8 +20,9 @@ my $rtc = MIDI::RtController->new(
 );
 
 my $rtf = MIDI::RtController::Filter::Tonal->new(rtc => $rtc);
-# $rtf->feedback(4);
-# $rtf->factor(1.2);
+
+$rtf->feedback(4);
+$rtf->factor(1.5);
 
 my $method = "curry::$filter_name";
 $rtc->add_filter($filter_name, [qw(note_on note_off)], $rtf->$method);
